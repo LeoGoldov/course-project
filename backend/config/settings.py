0aa6be7fc7c966746725e230c622bf1caed282c8
Вячеслав Жерлицын <gerlicin.vyacheslav1960@outlook.com>
2026-05-31 12:59:02 +0300

    Настроить Channel Layers для WebSocket

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -123,3 +123,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    }
+}
