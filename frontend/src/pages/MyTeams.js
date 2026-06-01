65e21bb183be71affdb5c934cacfe14f17355b5f
Вячеслав Жерлицын <gerlicin.vyacheslav1960@outlook.com>
2026-06-02 00:44:50 +0300

    удалить неиспользуемый импорт useAuth

--- frontend/src/pages/MyTeams.js
+++ frontend/src/pages/MyTeams.js
@@ -1,7 +1,6 @@
 // frontend/src/pages/MyTeams.js
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
 import { useMyTeams, useDeleteTeam } from '../hooks/useTeamsQuery';
 
 function MyTeams() {
